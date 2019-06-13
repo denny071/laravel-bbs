af3b887b26af65a37c0bde0ec696a5e6a23624a1
xiaoming <917942168@qq.com>
2019-06-13 04:53:32 +0000

    用户个人页面原型

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * 用户详情
+     *
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(User $user)
+    {
+        return view("users.show",compact("user"));
+    }
+}
