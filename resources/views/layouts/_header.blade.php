db51ac7b9090609af1db6fb4f2f3a08d67887aa3
xiaoming <917942168@qq.com>
2019-06-19 08:37:11 +0000

    用户管理后台

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -42,7 +42,7 @@
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               @can('manage_contents')
-                <a class="dropdown-item" href="">
+                <a class="dropdown-item" href="{{url(config('administrator.uri'))}}">
                   <i class="fas fa-tachometer-alt mr-2"></i>
                   管理后台
                 </a>
