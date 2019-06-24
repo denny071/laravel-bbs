3371a7ba0ac89a52645742bb7ca56d258475a956
xiaoming <917942168@qq.com>
2019-06-24 07:15:10 +0000

    rate limit

---
+++ config/api.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denny
+ * Date: 2019-06-24
+ * Time: 14:50
+ */
+
+
+return [
+    'rate_limits' => [
+        'expires' => [
+            'expires' => env('RATE_LIMITS_EXPIRES', 1),
+            'limit' => env('RATE_LIMITS', 60),
+        ],
+        'sign' => [
+            'expires' => env('SIGN_RATE_LIMITS_EXPIRES', 1),
+            'limit' => env('SIGN_RATE_LIMITS', 10),
+        ]
+    ],
+];
