f630159a68f099b76be65de32b8322af1cde233e
xiaoming <917942168@qq.com>
2019-07-02 08:44:07 +0000

    topic index

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,9 @@ $api->version('v1',[
         // 游客可以访问的接口
         $api->get('categories','CategoriesController@index')->name('api.categories.index');
 
+        $api->get('topics','TopicsController@index')->name('api.topics.index');
 
+        $api->get('user/{user}/topics','TopicsController@userIndex')->name('api.user.topics.index');
 
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
