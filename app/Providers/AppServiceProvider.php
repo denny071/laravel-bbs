d908eb1c295e36327576385657c1a981c854fcac
xiaoming <917942168@qq.com>
2019-07-02 08:07:30 +0000

    topic update api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,12 @@ class AppServiceProvider extends ServiceProvider
             //注册SudoSu插件
             $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
         }
+        \API::error(function(\Illuminate\Database\Eloquent\ModelNotFoundException $exception){
+            abort(404);
+        });
+        \API::error(function (\Illuminate\Auth\Access\AuthorizationException $exception) {
+            abort(403, $exception->getMessage());
+        });
     }
 
     /**
