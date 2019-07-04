bad9df194fa0dedb2bf23b49e42336cc75c71888
xiaoming <917942168@qq.com>
2019-07-04 05:06:02 +0000

    change jwt to passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -7,24 +7,65 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
+use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response as Psr7Response;
 
 class AuthorizationsController extends Controller
 {
-    public function store(AuthorizationRequest $request)
+//    public function store(AuthorizationRequest $request)
+//    {
+//        $username = $request->username;
+//
+//        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+//            $credentials['email'] = $username :
+//            $credentials['phone'] = $username;
+//
+//        $credentials['password'] = $request->password;
+//
+//        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+//            return $this->response->errorUnauthorized(trans('auth.failed'));
+//        }
+//
+//        return $this->respondWithToken($token);
+//    }
+
+//    public function update()
+//    {
+//        $token = Auth::guard('api')->refresh();
+//        return $this->respondWithToken($token);
+//    }
+
+
+//    public function destroy()
+//    {
+//        Auth::logout();
+//        return $this->response->noContent();
+//    }
+
+    public function store(AuthorizationRequest $originRequest, AuthorizationServer $server,ServerRequestInterface $serverRequest)
     {
-        $username = $request->username;
-
-        filter_var($username, FILTER_VALIDATE_EMAIL) ?
-            $credentials['email'] = $username :
-            $credentials['phone'] = $username;
-
-        $credentials['password'] = $request->password;
-
-        if (!$token = \Auth::guard('api')->attempt($credentials)) {
-            return $this->response->errorUnauthorized(trans('auth.failed'));
+        try {
+            return $server->respondToAccessTokenRequest($serverRequest, new Psr7Response)->withStatus(201);
+        }catch (OAuthServerException $e) {
+            return $this->response->errorUnauthorized($e->getMessage());
         }
+    }
 
-        return $this->respondWithToken($token);
+    public function update(AuthorizationServer $server, ServerRequestInterface $serverRequest)
+    {
+        try {
+            return $server->respondToAccessTokenRequest($serverRequest, new Psr7Response);
+        } catch (OAuthServerException $e) {
+            return $this->response->errorUnauthorized($e->getMessage());
+        }
+    }
+
+    public function destroy()
+    {
+        $this->user()->token()->revoke();
+        return $this->response->noContent();
     }
 
     public function socialStore($type, SocialAuthorizationRequest $request)
@@ -69,17 +110,7 @@ class AuthorizationsController extends Controller
         return $this->respondWithToken($token);
     }
 
-    public function update()
-    {
-        $token = Auth::guard('api')->refresh();
-        return $this->respondWithToken($token);
-    }
 
-    public function destroy()
-    {
-        Auth::logout();
-        return $this->response->noContent();
-    }
 
     protected function respondWithToken($token)
     {
--- config/api.php
+++ config/api.php
@@ -19,6 +19,7 @@ return [
         ]
     ],
     'auth' => [
-        'jwt' => 'Dingo\Api\Auth\Provider\JWT',
+//        'jwt' => 'Dingo\Api\Auth\Provider\JWT',
+        'oauth' => \App\Providers\PassportDingoProvider::class,
     ],
 ];
