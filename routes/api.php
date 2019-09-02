2a028b3835be70c59137e2bde1737d8e17f91f52
denny <917942168@qq.com>
2019-09-02 11:45:53 +0800

    no message

--- routes/api.php
+++ routes/api.php
@@ -101,6 +101,8 @@ $api->version('v1',[
             $api->get('user/notifications/stats', 'NotificationsController@stats')->name('api.user.notifications.stats');
             // 标记消息通知为已读
             $api->patch('user/read/notifications','NotificationsController@read')->name('api.user.notifications.read');
+             // 标记消息通知为已读
+            $api->put('user/read/notifications','NotificationsController@read')->name('api.user.notifications.read.put');
             // 当前登录用户权限
             $api->get('user/permissions','PermissionsController@index')->name('api.user.permissions.index');
 
